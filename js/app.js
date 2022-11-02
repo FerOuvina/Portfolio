78a179e2a5410066169d4bb562afa10e9e4d1717
Fernando Ouviña <fernandoouvina07@gmail.com>
2022-11-02 11:41:14 -0300

    adding proyects and footer section

--- js/app.js
+++ js/app.js
@@ -1,35 +1,60 @@
-const autoScroll = () => {
-  let proyects = document.querySelector('[data-proyects]');
-  setTimeout(() => {
-    proyects.scrollIntoView();
-  }, 10000);
-};
-autoScroll();
+// First auto scroll to proyects section after animation for presentation
+// const autoScroll = () => {
+//   let proyects = document.querySelector('[data-proyects]');
+//   setTimeout(() => {
+//     proyects.scrollIntoView();
+//   }, 10000);
+// };
+// autoScroll();
 
-const proyect1 = document.querySelector('[data-proyect1]');
-const proyect2 = document.querySelector('[data-proyect2]');
-const proyect3 = document.querySelector('[data-proyect3]');
+// // IntersectionObserver for proyects fade in/out
+// const proyect1 = document.querySelector('[data-proyect1]');
+// const proyect2 = document.querySelector('[data-proyect2]');
+// const proyect3 = document.querySelector('[data-proyect3]');
 
-const showProyect = (entries) => {
-  entries.forEach((entries) => {
-    if (entries.isIntersecting) {
-      entries.target.style.opacity = 1;
-    } else {
-      entries.target.style.opacity = 0;
-    }
-  });
-};
+// const showProyect = (entries) => {
+//   entries.forEach((entries) => {
+//     if (entries.isIntersecting) {
+//       entries.target.style.opacity = 1;
+//     } else {
+//       entries.target.style.opacity = 0;
+//     }
+//   });
+// };
 
-const observer = new IntersectionObserver(showProyect, {
-  root: null,
-  rootMargin: '0px',
-  threshold: 1.0
-});
-observer.observe(proyect1);
-observer.observe(proyect2);
-observer.observe(proyect3);
+// const observer = new IntersectionObserver(showProyect, {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 1.0
+// });
+// observer.observe(proyect1);
+// observer.observe(proyect2);
+// observer.observe(proyect3);
 
+// Second auto scroll from proyect to proyect
+// const autoScrollProyect1 = () => {
+//   let proyect1 = document.querySelector('[data-proyect1]');
+//   setTimeout(() => {
+//     proyect1.scrollIntoView();
+//   }, 15000);
+// };
+// autoScrollProyect1();
 
+// const autoScrollProyect2 = () => {
+//   let proyect2 = document.querySelector('[data-proyect2]');
+//   setTimeout(() => {
+//     proyect2.scrollIntoView();
+//   }, 25000);
+// };
+// autoScrollProyect2();
+
+// const autoScrollProyect3 = () => {
+//   let proyect3 = document.querySelector('[data-proyect3]');
+//   setTimeout(() => {
+//     proyect3.scrollIntoView();
+//   }, 35000);
+// };
+// autoScrollProyect3();
 
 
 
