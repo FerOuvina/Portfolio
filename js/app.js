761c7ced702e5f5cc672736071055c31c873ca37
Fernando Ouviña <fernandoouvina07@gmail.com>
2022-11-08 14:33:00 -0300

    adding more animations

--- js/app.js
+++ js/app.js
@@ -6,73 +6,156 @@ const moveIntoView = () => {
 window.addEventListener('load', moveIntoView());
 
 // First auto scroll to proyects section after animation for presentation
+const proyects = document.querySelector('[data-proyects]');
 const autoScroll = () => {
-  let proyects = document.querySelector('[data-proyects]');
   setTimeout(() => {
     proyects.scrollIntoView();
   }, 10000);
 };
 autoScroll();
 
-// IntersectionObserver for proyects fade in/out
-const proyect1 = document.querySelector('[data-proyect1]');
-const proyect2 = document.querySelector('[data-proyect2]');
-const proyect3 = document.querySelector('[data-proyect3]');
-
-const showProyect = (entries) => {
+// Proyects Title Animation
+const proyectsTitle = document.querySelector('[data-proyectsTitle]');
+const showProyectsTitle = (entries) => {
   entries.forEach((entries) => {
     if (entries.isIntersecting) {
-      gsap.fromTo(entries.target, {opacity: 0, x: '9000'}, {opacity: 1, x: 0, ease: 'circ'});
-    } else {
-      gsap.fromTo(entries.target, {opacity: 1}, {opacity: 0});
-    }
+      gsap.fromTo(entries.target, {opacity: 0, x: '-80000'}, {opacity: 1, x: 0, ease: 'circ'});
+      observerProyectsTitle.disconnect();
+    };
   });
 };
-
-const observer = new IntersectionObserver(showProyect, {
+const observerProyectsTitle = new IntersectionObserver(showProyectsTitle, {
   root: null,
   rootMargin: '0px',
   threshold: 1.0
 });
-observer.observe(proyect1);
-observer.observe(proyect2);
-observer.observe(proyect3);
+observerProyectsTitle.observe(proyectsTitle);
+
+// IntersectionObserver for proyects fade in
+const proyect1 = document.querySelector('[data-proyect1]');
+const proyect2 = document.querySelector('[data-proyect2]');
+const proyect3 = document.querySelector('[data-proyect3]');
+const video1 = document.querySelector('[data-video1]');
+const video2 = document.querySelector('[data-video2]');
+const video3 = document.querySelector('[data-video3]');
+const video4 = document.querySelector('[data-video4]');
+const showProyect1 = (entries) => {
+  entries.forEach((entries) => {
+    if (entries.isIntersecting) {
+      gsap.fromTo(entries.target, {opacity: 0, x: '90000'}, {opacity: 1, x: 0, ease: 'circ'});
+      gsap.fromTo(video1, {opacity: 0, x: '90000'}, {opacity: 1, x: -50, ease: 'circ'});
+      observerProyect1.disconnect();
+    };
+  });
+};
+const observerProyect1 = new IntersectionObserver(showProyect1, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.7
+});
+observerProyect1.observe(proyect1);
+
+const showProyect2 = (entries) => {
+  entries.forEach((entries) => {
+    if (entries.isIntersecting) {
+      gsap.fromTo(entries.target, {opacity: 0, x: '-90000'}, {opacity: 1, x: 0, ease: 'circ'});
+      observerProyect2.disconnect();
+    };
+  });
+};
+const observerProyect2 = new IntersectionObserver(showProyect2, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.7
+});
+observerProyect2.observe(proyect2);
+
+const showProyect3 = (entries) => {
+  entries.forEach((entries) => {
+    if (entries.isIntersecting) {
+      gsap.fromTo(entries.target, {opacity: 0, x: '90000'}, {opacity: 1, x: 0, ease: 'circ'});
+      observerProyect3.disconnect();
+    };
+  });
+};
+const observerProyect3 = new IntersectionObserver(showProyect3, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.7
+});
+observerProyect3.observe(proyect3);
+
+// Videos Animation
+// const showVideo1 = (entries) => {
+//   entries.forEach((entries) => {
+//     if (entries.isIntersecting) {
+//       gsap.fromTo(video1, {opacity: 0, x: '90000'}, {opacity: 1, x: -50, ease: 'circ'});
+//       observerVideo1.disconnect();
+//       gsap.fromTo(video4, {opacity: 0, x: '90000'}, {opacity: 1, x: -50, ease: 'circ'});
+//     };
+//   });
+// };
+// const observerVideo1 = new IntersectionObserver(showVideo1, {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 0.7
+// });
+// observerVideo1.observe(proyect1);
+// observerVideo1.observe(proyect2);
+// observerVideo1.observe(proyect3);
+
+// const showVideo2 = (entries) => {
+//   entries.forEach((entries) => {
+//     if (entries.isIntersecting) {
+//       gsap.fromTo(video2, {opacity: 0, x: '-90000'}, {opacity: 1, x: 40, y: -30, ease: 'circ'});
+//       gsap.fromTo(video3, {opacity: 0, x: '-90000'}, {opacity: 1, x: 40, y: -30, ease: 'circ'});
+//       observerVideo2.disconnect();
+//     };
+//   });
+// };
+// const observerVideo2 = new IntersectionObserver(showVideo2, {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 0.7
+// });
+// observerVideo2.observe(proyect1);
+// observerVideo2.observe(proyect2);
+// observerVideo2.observe(proyect3);
 
 // Second auto scroll from proyect to proyect
-const autoScrollProyect1 = () => {
-  setTimeout(() => {
-    proyect1.scrollIntoView({
-      behavior: "smooth",
-      block: "center"
-    });
-  }, 10000);
-};
-autoScrollProyect1();
+// const autoScrollProyect1 = () => {
+//   setTimeout(() => {
+//     proyect1.scrollIntoView({
+//       behavior: "smooth",
+//       block: "center"
+//     });
+//   }, 10000);
+// };
+// autoScrollProyect1();
 
-const autoScrollProyect2 = () => {
-  setTimeout(() => {
-    proyect2.scrollIntoView({
-      behavior: "smooth",
-      block: "center"
-    });
-  }, 20000);
-};
-autoScrollProyect2();
+// const autoScrollProyect2 = () => {
+//   setTimeout(() => {
+//     proyect2.scrollIntoView({
+//       behavior: "smooth",
+//       block: "center"
+//     });
+//   }, 20000);
+// };
+// autoScrollProyect2();
 
-const autoScrollProyect3 = () => {
-  setTimeout(() => {
-    proyect3.scrollIntoView({
-      behavior: "smooth",
-      block: "center"
-    });
-  }, 30000);
-};
-autoScrollProyect3();
+// const autoScrollProyect3 = () => {
+//   setTimeout(() => {
+//     proyect3.scrollIntoView({
+//       behavior: "smooth",
+//       block: "center"
+//     });
+//   }, 30000);
+// };
+// autoScrollProyect3();
 
 // Showing the navbar
 const contact = document.querySelector('[data-contact]');
 const navbar = document.querySelector('[data-navbar]');
-
 const showNavbar = (event) => {
   event.forEach((event) => {
     if (event.isIntersecting) {
@@ -121,3 +204,47 @@ const observerNavbarLeft = new IntersectionObserver(moveNavbarLeft, {
 });
 observerNavbarLeft.observe(proyect1);
 observerNavbarLeft.observe(proyect3);
+
+// Abot Me Animation
+const aboutMe = document.querySelector('[data-aboutMe]');
+const showAboutMe = (event) => {
+  event.forEach((event) => {
+    if (event.isIntersecting) {
+      gsap.fromTo(aboutMe, {opacity: 0, x: '90000'}, {opacity: 1, x: 0, ease: 'circ'});
+      observerAboutMe.disconnect()
+    };
+  });
+};
+const observerAboutMe = new IntersectionObserver(showAboutMe, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1.0
+});
+observerAboutMe.observe(aboutMe);
+
+// Skills Animation
+const skillsTitle = document.querySelector('[data-skillsTitle]');
+const htmlIcon = document.querySelector('[data-html]');
+const cssIcon = document.querySelector('[data-css]');
+const jsIcon = document.querySelector('[data-js]');
+const reactIcon = document.querySelector('[data-react]');
+const gitIcon = document.querySelector('[data-git]');
+const showIcons = (event) => {
+  event.forEach((event) => {
+    if (event.isIntersecting) {
+      gsap.fromTo(skillsTitle, {opacity: 0, x: '2000'}, {opacity: 1, x: 0, ease: 'circ', delay: 2, duration: 1});
+      gsap.fromTo(htmlIcon, {opacity: 0, x: '10000'}, {opacity: 1, x: 0, ease: 'circ', duration: 1});
+      gsap.fromTo(cssIcon, {opacity: 0, x: '8000'}, {opacity: 1, x: 0, ease: 'circ', duration: 1});
+      gsap.fromTo(jsIcon, {opacity: 0, x: '6000'}, {opacity: 1, x: 0, ease: 'circ', duration: 1});
+      gsap.fromTo(reactIcon, {opacity: 0, x: '4000'}, {opacity: 1, x: 0, ease: 'circ', duration: 1});
+      gsap.fromTo(gitIcon, {opacity: 0, x: '2000'}, {opacity: 1, x: 0, ease: 'circ', duration: 1});
+      observerIcons.disconnect();
+    };
+  });
+};
+const observerIcons = new IntersectionObserver(showIcons, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1.0
+});
+observerIcons.observe(aboutMe);
