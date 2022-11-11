6df5f2c75a070f839d281e94a213cbdafde3f2b2
Fernando Ouviña <fernandoouvina07@gmail.com>
2022-11-10 21:32:12 -0300

    adding a loading screen and animation for desktop users

--- js/attention.js
+++ js/attention.js
@@ -1,6 +1,18 @@
+// Timer for redirectioning to the portfolio site
 const timer = () => {
   setTimeout(() => {
     location.assign('./Portfolio.html');
-  }, 7000);
+  }, 6800);
 }
 window.addEventListener('load', timer());
+
+// Showing the animation on desktop or the welcome screen on mobile
+const main = document.querySelector('.main');
+const loadingContainer = document.querySelector('.loading__container');
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  main.style.display = 'flex';
+  loadingContainer.style.display = 'none';
+} else {
+  main.style.display = 'none';
+  loadingContainer.style.display = 'flex';
+};
